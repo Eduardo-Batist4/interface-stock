74bf4dcbd92824f31fec6a7bea3fa2df10f727f6
Eduardo-Oreia <carloswe3st@gmail.com>
2024-04-18 19:28:00 -0300

    header e botões de rota

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
 
 function App() {
   return (
-    <>
-      <h1>menu</h1>
+    <main className="bg-zinc-800 w-full h-screen">
+      <header className="bg-red-500 2-full h-24 flex justify-around items-center">
+          <h2 className="text-4xl font-bold uppercase">stock</h2>
+          <div className="space-x-7">
+            <Link to="/register" className="text-2xl">cadastrar</Link>
+            <Link to="/stock" className="text-2xl">estoque</Link>
+          </div>
+      </header>
       <Outlet />
-    </>
+    </main>
   )
 }
 
