57335e5ac26099884d135fb95220244ec39f13b2
Eduardo-Oreia <carloswe3st@gmail.com>
2024-04-18 19:45:15 -0300

    componente button feito

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { Link, Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom"
+import Button from "./components/button"
 
 function App() {
   return (
     <main className="bg-zinc-800 w-full h-screen">
-      <header className="bg-red-500 2-full h-24 flex justify-around items-center">
-          <h2 className="text-4xl font-bold uppercase">stock</h2>
+      <header className=" 2-full h-24 flex justify-around items-center">
+          <h2 className="text-4xl text-slate-50 font-bold uppercase">stock</h2>
           <div className="space-x-7">
-            <Link to="/register" className="text-2xl">cadastrar</Link>
-            <Link to="/stock" className="text-2xl">estoque</Link>
+            <Button text="cadastro" link="/register" />
+            <Button text="estoque" link="/stock" />
           </div>
       </header>
       <Outlet />
---
+++ src/components/button.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Button(props) {
+    return (
+        <>
+            <button className="bg-blue-700 px-4 py-1 rounded-md">
+                <Link to={props.link} className="text-slate-50 text-xl uppercase font-medium">{props.text}</Link>
+            </button>
+        </>
+    );
+};
+
+export default Button;
