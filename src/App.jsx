e52dbde7b230c71f0d8c19d30773133aec9b2b7b
Eduardo-Oreia <carloswe3st@gmail.com>
2024-04-18 21:46:57 -0300

    ajustando link do logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
 import Button from "./components/Button"
 
 function App() {
   return (
     <main className="bg-zinc-900 w-full h-screen">
       <header className=" 2-full h-24 flex justify-around items-center">
-          <h2 className="text-4xl text-slate-50 font-bold uppercase">stock</h2>
+          <Link>
+            <h2 className="text-4xl text-slate-50 font-bold uppercase">stock</h2>
+          </Link>
           <div className="space-x-7">
             <Button text="cadastro" link="/register" />
             <Button text="estoque" link="/stock" />
