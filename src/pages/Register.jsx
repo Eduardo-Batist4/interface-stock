e72aeb7241e15bb341b399f0fd2a243aa5e06099
Eduardo-Oreia <carloswe3st@gmail.com>
2024-04-24 14:32:15 -0300

    aviso cadastro com sucesso

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,6 +3,15 @@ import { useState } from "react";
 export function Register() {
     const [ nameProduct, setNameProduct ] = useState("");
     const [ quantity, setQuatity ] = useState(0);  
+    const [ successRegister, setSuccessRegister ] = useState("hidden")
+
+    const timer = () => {
+        setTimeout(() => {
+            setSuccessRegister("hidden")
+        }, 4000)
+        setSuccessRegister("block")
+    }
+
 
     // Criar o objeto com os valores calculados
     let data = {
@@ -26,7 +35,7 @@ export function Register() {
             if(!response.ok) {
                 throw new Error("Request error.");
             };
-    
+            timer()
         } catch (error) {
             console.error(error);
         }
@@ -35,29 +44,34 @@ export function Register() {
     }   
 
     return (
-        <section className="bg-slate-600/20 backdrop-blur-sm w-3/12 h-auto mx-auto p-5 text-center rounded-xl">
-            <h2 className="text-3xl uppercase font-bold text-slate-50">cadastro</h2>
-            <div className="text-center mt-10">
-                <form onSubmit={handleClick}>   
-                    <div className="text-left space-y-3 mb-5">
-                        <label htmlFor="name"className="text-xl uppercase font-medium text-slate-50">nome</label>
-                        <input type="text" name="name" 
-                        className="w-full font-medium outline-none px-4 py-3 rounded-lg"
-                        value={nameProduct}
-                        onChange={(ev) => setNameProduct(ev.target.value)}
-                        />                        
-                    </div>
-                    <div className="text-left space-y-3">
-                        <label htmlFor="quantity" className="text-xl uppercase font-medium text-slate-50">quantidade</label>
-                        <input type="number" name="quantity" 
-                        className="w-full font-medium outline-none px-4 py-3 rounded-lg"
-                        value={quantity}
-                        onChange={(ev) => setQuatity(ev.target.value)}
-                        />
-                    </div>
-                        <button className="bg-blue-700 w-80 px-8 py-3 rounded-xl text-slate-50 text-xl uppercase font-medium mt-10">cadastrar</button>
-                </form>
+        <>
+            <section className="static bg-slate-600/20 backdrop-blur-sm w-3/12 h-auto mx-auto p-5 text-center rounded-xl">
+                <h2 className="text-3xl uppercase font-bold text-slate-50">cadastro</h2>
+                <div className="text-center mt-10">
+                    <form onSubmit={handleClick}>   
+                        <div className="text-left space-y-3 mb-5">
+                            <label htmlFor="name"className="text-xl uppercase font-medium text-slate-50">nome</label>
+                            <input type="text" name="name" 
+                            className="w-full font-medium outline-none px-4 py-3 rounded-lg"
+                            value={nameProduct}
+                            onChange={(ev) => setNameProduct(ev.target.value)}
+                            />                        
+                        </div>
+                        <div className="text-left space-y-3">
+                            <label htmlFor="quantity" className="text-xl uppercase font-medium text-slate-50">quantidade</label>
+                            <input type="number" name="quantity" 
+                            className="w-full font-medium outline-none px-4 py-3 rounded-lg"
+                            value={quantity}
+                            onChange={(ev) => setQuatity(ev.target.value)}
+                            />
+                        </div>
+                            <button className="bg-blue-700 w-80 px-8 py-3 rounded-xl text-slate-50 text-xl uppercase font-medium mt-10">cadastrar</button>
+                    </form>
+                </div>
+            </section>
+            <div className={`bg-lime-400/10 border-solid border-2 border-lime-500/60 w-80 p-1 text-center absolute bottom-10 right-10 ${successRegister}`}>
+                <p className="text-slate-50">Produto Cadastrado com Sucesso!</p>
             </div>
-        </section>
+        </>
     )
 }   
